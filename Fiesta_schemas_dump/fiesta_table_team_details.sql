a193fff993c792e7dd32156f13ffc1582bf05463
srishilesh <srishilesh@gmail.com>
2020-12-01 12:35:18 +0530

    Fix sql schema dump - team details

--- Fiesta_schemas_dump/fiesta_table_team_details.sql
+++ Fiesta_schemas_dump/fiesta_table_team_details.sql
@@ -18,19 +18,14 @@ USE `fiesta`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `table_team_details`
+-- Dumping data for table `table_team_details`
 --
 
-DROP TABLE IF EXISTS `table_team_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `table_team_details` (
-  `team_id` int NOT NULL,
-  `team_name` varchar(100) NOT NULL,
-  `student_id` int NOT NULL,
-  PRIMARY KEY (`team_id`,`student_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `table_team_details` WRITE;
+/*!40000 ALTER TABLE `table_team_details` DISABLE KEYS */;
+INSERT INTO `table_team_details` VALUES (6,'Team Iris',12),(7,'Team Iris',13),(7,'Team Iris',14);
+/*!40000 ALTER TABLE `table_team_details` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -41,4 +36,4 @@ CREATE TABLE `table_team_details` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-01 11:05:32
+-- Dump completed on 2020-12-01 12:34:32
